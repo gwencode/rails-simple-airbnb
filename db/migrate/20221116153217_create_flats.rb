22f746ad8a11efa9755e18e231763d21a073d331
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-16 17:02:09 +0100

    begin of simple airbnb

---
+++ db/migrate/20221116153217_create_flats.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Create Flats Model
+class CreateFlats < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flats do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+      t.integer :price_per_night
+      t.integer :number_of_guests
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Amazing flat in Montmartre',
+  address: '135 rue Lamarck 75018 Paris',
+  description: 'A beautiful 10min walks from the Sacré Coeur',
+  price_per_night: 50,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Disgusting flat',
+  address: 'Clement street 56 Vannes',
+  description: "Don't enjoy every moment",
+  price_per_night: 10,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Rodolphe House',
+  address: '7 street of your ass',
+  description: 'A lot of farts',
+  price_per_night: 28,
+  number_of_guests: 10
+)
